a1adf36276cc1a047670eca146efd41cc2994c0e
Viktor Griniushin <141779925+Vikt0rG@users.noreply.github.com>
2026-08-06 22:23:20 +0200

    feat(badge): add badge re-flashing upon frequent calls

--- src/services/badge.js
+++ src/services/badge.js
@@ -1,10 +1,22 @@
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
+let currentRequestId = 0; // Track current request ID to manage badge updates
+
 export async function flashBadge(text, color) {
+    const requestId = ++currentRequestId;
+    console.log('Request ID:', requestId);
+
+    await chrome.action.setBadgeText({ text: '' });
+    await delay(100);
+
+    if (requestId !== currentRequestId) return;
+
     await chrome.action.setBadgeText({ text });
     await chrome.action.setBadgeBackgroundColor({ color });
 
-    await delay(1600);
+    await delay(3000);
 
-    await chrome.action.setBadgeText({ text: '' });
+    if (requestId === currentRequestId) {
+        await chrome.action.setBadgeText({ text: '' });
+    }
 }
